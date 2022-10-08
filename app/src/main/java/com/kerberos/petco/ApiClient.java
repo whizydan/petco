490de7b3a0b56a445beff21728387bfc206cb4d3
kerberos <whizydan@gmail.com>
2022-10-08 14:00:18 +0300

    Initial commit

---
+++ app/src/main/java/com/kerberos/petco/ApiClient.java
@@ -0,0 +1,27 @@
+package com.kerberos.petco;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by haerul on 15/03/18.
+ */
+
+class ApiClient {
+
+    private static final String BASE_URL = "https://api.kerberos.co.ke/apps/demos/";
+    private static Retrofit retrofit;
+
+    static Retrofit getApiClient(){
+
+        if (retrofit==null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+        return retrofit;
+    }
+
+}
